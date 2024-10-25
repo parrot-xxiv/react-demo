311d4adb6258786b907fba8e70037eaee4dfebfa
parrot-xxiv <parrot-xxiv@users.noreply.github.com>
2024-10-25 17:20:04 +0800

    add routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from '@/App.jsx'
+import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom'
+import * as React from 'react';
+import UseStatePage from '@/components/component/use-state.jsx';
+import UseEffectPage from '@/components/component/use-effect.jsx';
+import CurrencyConverter from '@/components/component/currency-converter';
+import NotFound from '@/components/component/not-found';
+import { ThemeProvider } from './components/component/theme-provider';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route path="react-hooks" element={<Outlet />} >
+              <Route path="use-state" element={<UseStatePage />} />
+              <Route path="use-effect" element={<UseEffectPage />} />
+            </Route>
+            <Route path="apps" element={<Outlet />} >
+              <Route path="currency-converter" element={<CurrencyConverter />} />
+            </Route>
+          </Route>
+          <Route path="*" element={<NotFound/>}/>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   </StrictMode>,
 )
