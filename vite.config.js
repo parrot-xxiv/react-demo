fb415cb7f0c74d09584ee0d26e7cde71ad5683b2
parrot-xxiv <parrot-xxiv@users.noreply.github.com>
2024-10-25 17:54:47 +0800

    modify base url

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://wxpmbqynklcnsm.com/react-demo/',
+  base: '/react-demo/',
   plugins: [react()],
   resolve: {
     alias: {
